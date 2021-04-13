8863dfa6c10f4de6010fd21592584e8bb93db8a1
taotao_glb <taotao_glb@icloud.com>
2021-04-14 01:38:43 +0800

    页面跳转 跨组件信息传递

--- src/components/productCollection/ProductItem.tsx
+++ src/components/productCollection/ProductItem.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Image, Typography } from 'antd'
+import { withRouter, RouteComponentProps } from 'react-router-dom'
 
-interface PropsType {
+interface PropsType extends RouteComponentProps {
 	id: string | number
 	size: 'large' | 'small'
 	imageSrc: string
@@ -9,15 +10,18 @@ interface PropsType {
 	title: string
 }
 
-export const ProductItem: React.FC<PropsType> = ({
+const ProductItemComponent: React.FC<PropsType> = ({
 	id,
 	size,
 	imageSrc,
 	price,
 	title,
+	history,
+	location,
+	match,
 }) => {
 	return (
-		<>
+		<div onClick={() => history.push(`/detail/${id}`)}>
 			{size === 'large' ? (
 				<Image src={imageSrc} width={460} height={285} />
 			) : (
@@ -31,6 +35,8 @@ export const ProductItem: React.FC<PropsType> = ({
 					¥ {price} 起
 				</Typography.Text>
 			</div>
-		</>
+		</div>
 	)
 }
+
+export const ProductItem = withRouter(ProductItemComponent)
