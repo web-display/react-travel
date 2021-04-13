577ac5831933be311df420b79a65a2ae7d6bb685
taotao_glb <taotao_glb@icloud.com>
2021-04-13 18:12:56 +0800

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,15 @@
 import React from 'react'
 import styles from './App.module.css'
-import {
-	Carousel,
-	Footer,
-	Header,
-	ProductCollection,
-	SideMenu,
-} from './components'
-import { Row, Col, Typography } from 'antd'
-
-import { productList1, productList2, productList3 } from './mockups'
-import sideImage from './assets/images/sider_2019_12-09.png'
-import sideImage2 from './assets/images/sider_2019_02-04.png'
-import sideImage3 from './assets/images/sider_2019_02-04-2.png'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { HomePage } from './pages'
 
 function App() {
 	return (
 		<div className={styles.App}>
-			<Header />
-			<div className={styles['page-content']}>
-				<Row style={{ marginTop: 20 }}>
-					<Col span={6}>
-						<SideMenu />
-					</Col>
-					<Col span={18}>
-						<Carousel />
-					</Col>
-				</Row>
-				<ProductCollection
-					title={
-						<Typography.Title level={3} type="warning">
-							爆款推荐
-						</Typography.Title>
-					}
-					sideImage={sideImage}
-					products={productList1}
-				/>
-				<ProductCollection
-					title={
-						<Typography.Title level={3} type="danger">
-							新品上市
-						</Typography.Title>
-					}
-					sideImage={sideImage2}
-					products={productList2}
-				/>
-				<ProductCollection
-					title={
-						<Typography.Title level={3} type="success">
-							国内游推荐
-						</Typography.Title>
-					}
-					sideImage={sideImage3}
-					products={productList3}
-				/>
-			</div>
-			<Footer />
+			<BrowserRouter>
+				<Route exact path="/" component={HomePage} />
+				<Route path="/signIn" render={() => <h1>登陆页面</h1>} />
+			</BrowserRouter>
 		</div>
 	)
 }
